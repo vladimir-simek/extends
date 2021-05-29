4b34a7c51dea111fe66f25b659a46d89b95784b7
vladimir-simek <minervladik@gmail.com>
2021-05-29 16:13:50 +0200

    circle done

--- src/com/vladimirsimek/extendstest/data/objects/Circle.java
+++ src/com/vladimirsimek/extendstest/data/objects/Circle.java
@@ -1,4 +1,27 @@
 package com.vladimirsimek.extendstest.data.objects;
 
-public class Circle {
+public class Circle extends Object {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.setArea(countArea(radius));
+        this.setPerimeter(countPerimeter(radius));
+    }
+
+    public double countPerimeter(double radius) {
+        return 2 * 3.14 * radius;
+    }
+
+    public double countArea(double radius) {
+        return (3.14) * radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
